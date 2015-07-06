982dd6946aa42c77fbe7d632107ca3b76917581d
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-06 15:57:56 -0600

    broken vector

--- bloom.rb
+++ bloom.rb
@@ -22,18 +22,66 @@ set[index3] = 1
 
 #p set
 #
+#class BloomFilter
+#  attr_accessor :element, :set
+#
+#  def initialize
+#    @set = [0,0,0,0,0,0,0,0]
+#  end
+#
+#  def add(element)
+#    digest(element)
+#    modulate
+#    binary
+#    return @set
+#  end
+#
+#  def digest(element)
+#    @d1 = Digest::MD5.hexdigest(element)
+#    @d2 = Digest::SHA1.hexdigest(element)
+#    @d3 = Digest::SHA2.hexdigest(element)
+#  end
+#
+#  def modulate
+#    @index1 = @d1.hex % @set.length
+#    @index2 = @d2.hex % @set.length
+#    @index3 = @d3.hex % @set.length
+#  end
+#
+#  def binary
+#    @set[@index1] = 1
+#    @set[@index2] = 1
+#    @set[@index3] = 1
+#  end
+#
+#  def presence?(element)
+#    filter = BloomFilter.new
+#    if filter.add(element) == @set
+#      puts "that element is in the set"
+#    else
+#      puts "I dunno if it's there"
+#    end
+#  end
+#end
+
+#bf = BloomFilter.new
+#bf.add("abc")
+#bf.add("overlap")
+#bf.presence?("abc")
+#bf.presence?("shampow")
+
 class BloomFilter
   attr_accessor :element, :set
 
   def initialize
-    @set = [0,0,0,0,0,0,0,0]
+    @set = "00000000".to_i(2)
   end
 
   def add(element)
     digest(element)
     modulate
     binary
-    return @set
+    print @set
   end
 
   def digest(element)
@@ -43,15 +91,15 @@ class BloomFilter
   end
 
   def modulate
-    @index1 = @d1.hex % @set.length
-    @index2 = @d2.hex % @set.length
-    @index3 = @d3.hex % @set.length
+    @index1 = @d1.hex % 8
+    @index2 = @d2.hex % 8
+    @index3 = @d3.hex % 8
   end
 
   def binary
-    @set[@index1] = 1
-    @set[@index2] = 1
-    @set[@index3] = 1
+    @set | "#{@index1}".to_i(2)
+    @set | "#{@index2}".to_i(2)
+    @set | "#{@index3}".to_i(2)
   end
 
   def presence?(element)
@@ -65,11 +113,4 @@ class BloomFilter
 end
 
 bf = BloomFilter.new
-bf.add("abc")
-print bf.set
-bf.add("overlap")
-print bf.set
-bf.presence?("abc")
-bf.presence?("shampow")
-
-00010111
+bf.add("wow")
