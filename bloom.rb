7ad3c281fcbd3cc45ff81d65909929ffe20193fc
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-06 15:29:36 -0600

    set indecing

--- bloom.rb
+++ bloom.rb
@@ -1,7 +1,9 @@
 require "digest"
 
 set = [0,0,0,0,0,0,0,0]
+
 element = "pizza"
+
 d1 = Digest::MD5.hexdigest(element)
 d2 = Digest::SHA1.hexdigest(element)
 d3 = Digest::SHA2.hexdigest(element)
@@ -13,4 +15,8 @@ index3 = d3.hex % set.length
 puts index1
 puts index2
 puts index3
+
+set[index1] = 1
+set[index2] = 2
+set[index3] = 3
 
