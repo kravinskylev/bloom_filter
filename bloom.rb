5419534e25692a965dafdade94a992c76bec4a52
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-06 15:28:42 -0600

    pizza modding

--- bloom.rb
+++ bloom.rb
@@ -1,5 +1,16 @@
-require 'digest'
+require "digest"
 
-Digest::MD5.hexdigest("yo")
-Digest::SHA1.hexdigest("yo")
-Digest::SHA2.hexdigest("yo")
+set = [0,0,0,0,0,0,0,0]
+element = "pizza"
+d1 = Digest::MD5.hexdigest(element)
+d2 = Digest::SHA1.hexdigest(element)
+d3 = Digest::SHA2.hexdigest(element)
+
+index1 = d1.hex % set.length
+index2 = d2.hex % set.length
+index3 = d3.hex % set.length
+
+puts index1
+puts index2
+puts index3
+
